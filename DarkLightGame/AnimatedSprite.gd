3fa9b6de90e9047eedec22740ee78354efe64759
ahmet9cengiz <ahcengiz@iu.edu>
2019-11-14 15:40:03 -0500

    basic sprite animation

--- DarkLightGame/AnimatedSprite.gd
+++ DarkLightGame/AnimatedSprite.gd
@@ -2,14 +2,31 @@ extends AnimatedSprite
 
 var timer
 
+#add a timer as a child
+#wait_time can be changed to make animation look better
 func _ready():
-	timer = Timer()
-	
-	pass
-	
+	timer = Timer.new()
+	add_child(timer)
+	timer.wait_time = 0.11
+	timer.start()
+
+#manipulate the animation style
 func _process(delta):
 	if Input.is_key_pressed(KEY_LEFT):
+		self.animation = "run"
+		timer.connect("timeout", self, "animateMe")
 		self.position.x -= 100 * delta
 	elif Input.is_key_pressed(KEY_RIGHT):
+		self.animation = "run"
+		timer.connect("timeout", self, "animateMe")
 		self.position.x += 100 * delta
-	
+	else:
+		self.animation = "idle"
+
+#loop through the frames
+func animateMe():
+	if self.frame < 9 :
+		self.frame += 1
+	elif self.frame == 9:
+		self.frame = 0
+
