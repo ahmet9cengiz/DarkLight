b984a91d1a4f3fcfd1b7b5fa4973a65cbb6c3f25
Matt Cobbs <mattcobbs@149-162-152-186.dhcp-in.iupui.edu>
2019-12-08 16:26:10 -0500

    Title Screen update

--- DarkLightGame/TitleScreen.gd
+++ DarkLightGame/TitleScreen.gd
@@ -5,3 +5,11 @@ func _ready():
 	self.add_child(audioStream)
 	audioStream.stream = load("res://audio/3880-hitman-by-kevin-macleod.ogg")
 	audioStream.play()
+
+
+func _on_Start_Game_button_pressed():
+	get_tree().change_scene("LevelOne.tscn")
+
+
+func _on_Quit_Button_pressed():
+	get_tree().quit()
