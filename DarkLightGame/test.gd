50b4c807c5b5a3b8141f27502b4d57369c9b578a
ahmet9cengiz <ahcengiz@iu.edu>
2019-12-09 22:23:03 -0500

    nothing new

--- DarkLightGame/test.gd
+++ DarkLightGame/test.gd
@@ -1,27 +1,25 @@
 extends Node2D
 
 onready var coin = preload("res://coin.tscn")
+
 var screensize
-var score = 0
 
 func _ready():
 	randomize()
 	screensize = get_viewport().get_size()
 	$"/root/globalScript".connect("game_over", self, "_on_game_over")
 	set_process(true)
-	spawnCoins(4)
+	spawnCoins(10)
 	
 func spawnCoins(number):
 	for i in range(number):
 		var c = coin.instance()
-		add_child(c)
+		$coinContainer.add_child(c)
 		c.connect("coin_grabbed", self, "_on_coin_grabbed")
-		c.position = Vector2(rand_range(50, screensize.x - 50), rand_range(screensize.y - 200, screensize.y - 50))
+		c.position = Vector2(rand_range(100,  2000), rand_range(screensize.y - 300, screensize.y - 100))
 
 func _on_coin_grabbed():
-	score += 10
-	print(score)
-
+	$"/root/globalScript".score += 10
 
 func _on_game_over():	
 	$Player.die()
